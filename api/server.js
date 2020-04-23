ee5afcd776a2a361cb1f44ad162b86e338d1ecd1
Jana Scheuble <janascheuble@Janas-MacBook-Air.local>
2020-04-23 16:28:33 -0700

    wrote tests for get/dogs endpoint

--- api/server.js
+++ api/server.js
@@ -1,11 +1,23 @@
 const express = require("express");
 
+const Dogs = require("../dogs/dog-model");
+
 const server = express();
 
 server.use(express.json());
 
 server.get("/", (req, res) => {
   res.status(200).json({ api: "works" });
+});
+
+server.get("/dogs", (req, res) => {
+  Dogs.getAll()
+    .then((dogs) => {
+      res.status(200).json(dogs);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err });
+    });
 });
 
 module.exports = server;
--- api/server.spec.js
+++ api/server.spec.js
@@ -14,4 +14,16 @@ describe("server.js", () => {
       expect(res.body.api).toBe("works");
     });
   });
+
+  describe("GET /dogs", () => {
+    it("should return a status of 200 OK", async () => {
+      const res = await request(server).get("/dogs");
+      expect(res.status).toBe(200);
+    });
+
+    it("returns an array of dogs", async () => {
+      let res = await request(server).get("/dogs");
+      expect(res.body.length).toBe(0);
+    });
+  });
 });
