a921f6059bbbc57072a1c1b8ad3747c28d4354d8
Jana Scheuble <janascheuble@Janas-MacBook-Air.local>
2020-04-23 13:24:46 -0700

    initial commit, simple server running

---
+++ knexfile.js
@@ -0,0 +1,42 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    client: "sqlite3",
+    connection: {
+      filename: "./data/database.db3",
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./data/migrations",
+    },
+    seeds: {
+      directory: "./data/seeds",
+    },
+  },
+
+  testing: {
+    client: "sqlite3",
+    connection: {
+      filename: "./data/test.db3",
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./data/migrations",
+    },
+    seeds: {
+      directory: "./data/seeds",
+    },
+  },
+
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL, // provided by Heroku
+    migrations: {
+      directory: "./data/migrations",
+    },
+    seeds: {
+      directory: "./data/seeds",
+    },
+  },
+};
